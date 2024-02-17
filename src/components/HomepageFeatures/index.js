f9b619fbaa1a7014f56997a9f16193ac62494db8
Shashank01729 <117083031+Shashank01729@users.noreply.github.com>
2024-02-18 04:13:25 +0530

    seventh

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,18 +8,18 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        PyDocs was designed from the ground up to be easily accessed and
+        used to get your clear your queries quickly.
       </>
     ),
-  },
+  },  
   {
     title: 'Focus on What Matters',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        PyDocs lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and leave your books on the <code>Rack</code> and dive in the virtual world .
       </>
     ),
   },
@@ -28,8 +28,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        PyDocs is made by using Docusaurus which is powered by React. AI Powered Searching Functionality is powered by DocSearch and Algolia.
       </>
     ),
   },
